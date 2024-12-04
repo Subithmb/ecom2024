fc41839329a1d09369029f5b0c11643190c5be6b
Subithmb <bijusubith@gmail.com>
2024-12-04 09:53:24 +0530

    prettier and lint fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,9 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  /* eslint-disable no-unused-vars */
   constructor(private readonly appService: AppService) {}
+  /* eslint-enable no-unused-vars */
 
   @Get()
   getHello(): string {
